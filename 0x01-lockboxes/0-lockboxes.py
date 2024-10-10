b04e27ca6f1b47d5db9ca4d90b923b6a6cf55efe
liusseka <cjmatheka@gmail.com>
2024-10-10 18:23:11 +0300

    Correct lock boxes task - alx interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,16 +2,17 @@
 
 """Task checks and uses keys to unlock boxes"""
 
+
 def canUnlockAll(boxes):
-    unlocked = set()  
-    unlocked.add(0)   
-    keys = [0]      
+    unlocked = set()
+    unlocked.add(0)
+    keys = [0]
 
     while keys:
-        current_box = keys.pop() 
+        current_box = keys.pop()
         for key in boxes[current_box]:
-            if key not in unlocked:  
-                unlocked.add(key)    
-                keys.append(key)     
+            if key not in unlocked:
+                unlocked.add(key)
+                keys.append(key)
 
     return len(unlocked) == len(boxes)
