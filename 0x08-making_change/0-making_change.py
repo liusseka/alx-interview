bf9f8444f190d5b93c31fb8414926a167320c2cf
Julius Charles <cjmatheka@gmail.com>
2024-12-01 22:56:04 +0300

    Correct Tasks

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Making a Change module.
+"""
+
+
+def makeChange(coins, total):
+    """DCalculates the minimum number of coins required to make
+    a specified total, given a collection of coins with
+    varying values..
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
